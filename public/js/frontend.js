bb595fb408be639035586022e9bc7289f325f80d
Devin Heigert <heigertd@gmail.com>
2020-04-23 10:17:10 -0700

    frontend routes

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,4 +1,3 @@
-//frontend javascript to make ajax calls 
 $('.message a').click(function() {
     $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
 });
@@ -8,3 +7,110 @@ var loadFile = function(event) {
     var image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+//frontend javascript to make ajax calls 
+$(function () {
+    //new user front end
+    $(".create").on("click", function (event) {
+        event.preventDefault();
+        let newUser = {
+            username: $(".name").val(),
+            password: $(".password").val(),
+        }
+
+        console.log(newUser)
+        $.ajax({
+            method: "POST",
+            data: newUser,
+            url: "/signup"
+        }).then(function () {
+            console.log("hello")
+        })
+    })
+
+    //new bracket
+    $(".submit-btn").on("click", function (event) {
+        event.preventDefault();
+        optionsArry = [];
+        option_one = $("#option_one").val().trim();
+        option_two = $("#option_two").val().trim();
+        option_three = $("#option_three").val().trim();
+        option_four = $("#option_four").val().trim();
+        option_five = $("#option_five").val().trim();
+        option_six = $("#option_six").val().trim();
+        option_seven = $("#option_seven").val().trim();
+        option_eight = $("#option_eight").val().trim();
+
+        optionsArry.push(option_one, option_two, option_three, option_four, option_five, option_six, option_seven, option_eight);
+
+        console.log(optionsArry)
+
+        let newBracket = {
+            name: $("#bracket_name").val().trim(),
+            options: optionsArry
+        }
+        $.ajax({
+            method: "POST",
+            data: newBracket,
+            url: "/new"
+        }).then(function () {
+            console.log("hello")
+        })
+    })
+
+    $(".vote").on("click", function (event) {
+        let votingFor = $(this).data("option")
+        let notVotingFor;
+        
+
+        if(votingFor === 1){
+           notVotingFor = 2
+        }else{
+            notVotingFor = 1
+        }
+
+        let votes= {
+            votingFor: votingFor,
+            notVotingFor: notVotingFor
+        }
+
+        console.log(votingFor)
+        console.log(notVotingFor)
+
+        $.ajax({
+            method: "PUT",
+            data: votes,
+            url: "/vote/" + id
+        }).then(function () {
+            console.log("hello")
+        })
+    })
+
+    //update round/ advance round
+    $(".end-rd").on("click", function (event) {
+        let nextRound = $(this).data("rd")
+        console.log(nextRound)
+
+        $.ajax({
+            method: "PUT",
+            data: nextRound,
+            url: "/nextround/" + id
+        }).then(function () {
+            console.log("hello")
+        })
+    })
+
+    //close bracket
+    $(".close-btn").on("click", function (event) {
+        let finalWinner = $("#winner").text()
+        console.log(finalWinner)
+
+        $.ajax({
+            method: "PUT",
+            data: finalWinner,
+            url: "/close/" + id
+        }).then(function () {
+            console.log("hello")
+        })
+    })
+})
